8a7b15326ff1d0266da6474b1451ea58b68c0d4c
Larry Mckuydee <larry.mckuydee@gmail.com>
2019-09-06 13:00:37 +0000

    Generator for laravel model

---
+++ generator/laravel/model.rb
@@ -0,0 +1,42 @@
+module Generator
+    module Laravel
+        module Model
+            extend self
+            def generate(args)
+                # ruby main.rb scaffold Model column:type,require_or_nullable:form_type
+                model, *args = args
+
+                data =
+                    File.read(
+                        "#{Dir.getwd}/template/laravel/model/model.stub"
+                    )
+                filename = "#{model}.php"
+
+                replaced_data = data.gsub 'ModelName', model
+
+                fillable_contents = ''
+
+                args.each do |arg|
+                    #split column:type:form_type
+                    variable = arg.split ':'
+                    column, type_and_require, form_type, *args = variable 
+                    data_type, nullable = type_and_require.split ','
+
+                    fillable_contents.concat "'#{column}',"
+                    fillable_contents.concat "\n            "
+                end
+
+                replaced_data = replaced_data.gsub 'FILLABLE_CONTENT', fillable_contents
+
+                open(filename, 'w') { |f|  
+                    f.write replaced_data  
+                }
+                puts "Laravel model generate complete. #{filename}"
+            end
+
+            def foo
+                puts 'PascalCase'.underscore
+            end
+        end
+    end
+end
--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require_relative "generator/laravel/migration"
 require_relative "generator/laravel/controller"
+require_relative "generator/laravel/model"
 require_relative "generator/vue/component"
 
 require 'active_support/all'
@@ -11,5 +12,6 @@ command, *args = ARGV
 if command=="scaffold"
     Generator::Laravel::Migration::generate(args) 
     Generator::Laravel::Controller::generate(args)
+    Generator::Laravel::Model::generate(args)
     Generator::Vue::Component::generate(args)
 end
